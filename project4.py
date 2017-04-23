fd9bc1b2d7db4be362d8be21b67f5345d1a1d581
Christopher Robert Philabaum <christopher@philabaum.me>
2017-04-22 20:45:55 -0700

    Filled in some missing leftovers from rebase

--- project4.py
+++ project4.py
@@ -12,7 +12,7 @@ class Board:
         self._size = size
         self._init_view()
 
-    def _init_view(self,):
+    def _init_view(self):
         self._view.grid()
         self.winArea1 = [[0, self._size - 1], [0, self._size - 2], [0, self._size - 3],
                          [0, self._size - 4],
@@ -25,25 +25,14 @@ class Board:
                          [self._size - 3, 0], [self._size - 3, 1],
                          [self._size - 4, 0]]
 
-        self._grid = self._init_grid()
-
-    def createBoard(self):
-        self.boardGrid.extend([[0]*self.boardSize]* self.boardSize)
-
-        for x in range(self.boardSize):
-            for y in range(self.boardSize):
-                if ([x, y] not in self.winArea1 and [x, y] not in self.winArea2):
-                    piece = tk.Button(bg=NORMAL_SQUARE_COLOR, width=40, height=40, text='empty', image=self.blankTile, borderwidth=5)
-                elif ([x, y] in self.winArea1):
-                    piece = tk.Button(bg=AREA1_SQUARE_COLOR, width=40, height=40, text='blue', image=self.blueBall, borderwidth=5)
-                else:
-                    piece = tk.Button(bg=AREA2_SQUARE_COLOR, width=40, height=40, text='red', image=self.redBall, borderwidth=5)
-                self.boardGrid[x][y] = piece
-                piece.grid(row=x, column=y)
-
         self._blank_tile = tk.PhotoImage(file='')
         self._blue_ball = tk.PhotoImage(file='blueBall.png')
         self._red_ball = tk.PhotoImage(file='redBall.png')
+
+        self._grid = self._init_grid()
+
+    def _init_grid(self):
+        grid = [[None] * self._size] * self._size
 
         for x in range(self._size):
             for y in range(self._size):
@@ -76,12 +65,13 @@ class Game:
         resetButton = tk.Button(text="RESTART", command=lambda:self.restart())
         resetButton.grid(row=self.boardSize+2, columnspan=self.boardSize)
 
+    def _add_quit_btn(self):
         # create a quit button to quit the game
         quitButton = tk.Button(text="QUIT", command=lambda:self.quit())
         quitButton.grid(row=self.boardSize+4, columnspan=self.boardSize)
 
     def restart(self):
-        self.createBoard()
+        self._board = Board(size)
 
     def move(self):
         #todo
