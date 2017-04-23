4516edc35c384851a936e1230fc56b66d8f7a182
Xiangzhi Cao <xc42@nau.edu>
2017-04-22 21:10:51 -0700

    finding legal moves

--- project4.py
+++ project4.py
@@ -70,6 +70,50 @@ class Game:
         quitButton = tk.Button(text="QUIT", command=lambda:self.quit())
         quitButton.grid(row=self.boardSize+4, columnspan=self.boardSize)
 
+    def findLegalMoves(self, board):
+        legalMoves = []
+
+        # first, we want to find all jump moves
+        for i in range(self.boardSize):
+            for j in range(self.boardSize):
+                if self.boardGrid[i][j].text != 'empty':
+                    self.findJump(board, [i, j], [i, j-1], legalMoves)
+                    self.findJump(board, [i, j], [i, j+1], legalMoves)
+                    self.findJump(board, [i, j], [i-1, j], legalMoves)
+                    self.findJump(board, [i, j], [i-1, j-1], legalMoves)
+                    self.findJump(board, [i, j], [i-1, j+1], legalMoves)
+                    self.findJump(board, [i, j], [i+1, j], legalMoves)
+                    self.findJump(board, [i, j], [i+1, j-1], legalMoves)
+                    self.findJump(board, [i, j], [i+1, j+1], legalMoves)
+
+        # if no jump moves have been found, then find regular moves
+        if legalMoves == []:
+            for i in range(self.boardSize):
+                for j in range(self.boardSize):
+                    if (self.boardGrid[i][j].text != 'empty'):
+                        self.findRegularMove(board, [i, j], legalMoves)
+
+    def findJump(self, board, current, transit, legalMoves):
+        if (transit[0] < 0 and transit >= self.boardSize):
+            return
+        for i in range(transit[0]-1, transit[0]+2):
+            for j in range(transit[1]-1, transit[1]+2):
+                if ((i - transit[0]) == (transit[0] - current[0]) and
+                            (j - transit[1]) == (transit[1] - current[1]) and
+                        (not (i == current[0] and j == current[1]))):
+                    if (board[i][j].text != 'empty'):
+                        legalMoves.append([i, j])
+
+
+    def findRegularMove(self, board, current, legalMoves):
+        for i in range(current[0]-1, current[0]+2):
+            for j in range(current[1]-1, current[1]+2):
+                if (i >= 0 and j >= 0 and i < self.boardSize and j < self.boardSize
+                and (not(current[0] == i and current[1] == j))):
+                    if (board[i][j].text != 'empty'):
+                        legalMoves.append([i, j])
+
+
     def restart(self):
         self._board = Board(size)
 
